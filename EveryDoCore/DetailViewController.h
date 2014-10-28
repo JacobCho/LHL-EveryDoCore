503e6efd8e6068396c7a904b3c3aefc978c5b237
Jacob Cho <jacobshcho@gmail.com>
2014-10-28 10:57:41 -0700

    Initial Commit

---
+++ EveryDoCore/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  EveryDoCore
+//
+//  Created by Jacob Cho on 2014-10-28.
+//  Copyright (c) 2014 Jacob Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ EveryDoCore/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  EveryDoCore
+//
+//  Created by Jacob Cho on 2014-10-28.
+//  Copyright (c) 2014 Jacob Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
